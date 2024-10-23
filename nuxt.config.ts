59050d70643e7f0406ef1f2de64518f106d5d3d2
Fabian Tenzara <fabiantenzara@gmail.com>
2024-10-23 09:55:39 +0700

    feat: deploying

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
-  app: {
-    head: {
-      title: 'Film Hunt - Discover your movie',
-      meta: [
-        { name: 'description', content: 'Discover your movie here!' }]
-    }
-  }
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image']
 })
